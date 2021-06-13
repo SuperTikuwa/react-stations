24ae81759af4b5121f020e758031044e941a4ec2
SuperTikuwa <tikuwamk2@gmail.com>
2021-06-13 15:43:54 +0900

    Station8

--- src/Description.js
+++ src/Description.js
@@ -3,15 +3,38 @@ import * as React from 'react'
 import { DogImage } from './DogImage'
 
 export const Description = () => {
+  const [dogURL, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/terrier-lakeland/n02095570_763.jpg',
+  )
+
   return (
     <div>
       <div className="page-content">
         <div className="app-text">
           <p>RailwayのReactコースで作成したページです．</p>
         </div>
-        <DogImage />
+        <div className="dog-image">
+          <DogImage url={dogURL} />
+          <div className="dog-image-button">
+            <button
+              onClick={async () => {
+                setDogUrl(await getRandomImage())
+              }}
+            >
+              Click Me
+            </button>
+          </div>
+        </div>
       </div>
       <hr />
     </div>
   )
+}
+
+async function getRandomImage() {
+  const res = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await res.json()
+  // const data = await res.json()
+
+  return data.message
 }
--- src/DogImage.js
+++ src/DogImage.js
@@ -1,30 +1,6 @@
 // DO NOT DELETE
 import * as React from 'react'
 
-export const DogImage = () => {
-  const [dogURL, setDogUrl] = React.useState(
-    'https://images.dog.ceo/breeds/terrier-lakeland/n02095570_763.jpg',
-  )
-  return (
-    <div className="dog-image">
-      <img src={dogURL}></img>
-      <div className="dog-image-button">
-        <button
-          onClick={async () => {
-            setDogUrl(await getRandomImage())
-          }}
-        >
-          Click Me
-        </button>
-      </div>
-    </div>
-  )
-}
-
-async function getRandomImage() {
-  const res = await fetch('https://dog.ceo/api/breeds/image/random')
-  const data = await res.json()
-  // const data = await res.json()
-
-  return data.message
+export const DogImage = props => {
+  return <img src={props.url}></img>
 }
